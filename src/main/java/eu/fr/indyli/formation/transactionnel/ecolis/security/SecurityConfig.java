1f0593332ee07c902743caec96839c1551fc71ec
abdou.zoyim <abdou.zoyim@indyli-services.com>
2024-02-23 08:38:42 +0100

    Test security con success

--- src/main/java/eu/fr/indyli/formation/transactionnel/ecolis/security/SecurityConfig.java
+++ src/main/java/eu/fr/indyli/formation/transactionnel/ecolis/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((request) -> request
 				.requestMatchers("/token/*", "/token/is-token-valid", "/signup")
-				.permitAll().anyRequest().permitAll())
+				.permitAll().anyRequest().authenticated())
 				.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class).build();
 	}
 
